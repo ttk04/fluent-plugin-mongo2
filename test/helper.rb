bedbe67d706a4fd0b9aebbca1e5b95aea5b9e1e9
Hiroshi Hatake <hatake@clear-code.com>
2016-05-25 15:54:59 +0900

    Add missing fluent/input require

--- test/helper.rb
+++ test/helper.rb
@@ -1,6 +1,7 @@
 require 'test/unit'
 require 'fluent/test'
 require 'mongo'
+require 'fluent/input'
 require 'fluent/plugin/out_mongo2'
 require 'fluent/plugin/out_mongo_replset2'
 require 'fluent/plugin/in_mongo_tail2'
